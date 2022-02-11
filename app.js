2e687e73bea936ba1ef1d7d1f39969372136e2c6
paskylag <91765555+paskylag@users.noreply.github.com>
2022-02-11 09:56:12 +0100

    Navigation

--- app.js
+++ app.js
@@ -1,39 +1,10 @@
-/*function username(){console.log('architecture','building', 'chemistry', 'art', 'commercial')}
-let input=document.querySelector('#input');
+let wrap=document.querySelector('wrap-btn');
 
- input.innerhtml='architecture','building','art', 'sciences';*/
+let open_btn=document.querySelector('open-btn');
 
- /*let username;
+let close_btn=document.querySelector('close-btn');
 
- document.querySelector('#input').innertext= username;
+open_btn.addEventListener('click', function(){for(x=0;x<wrap.length;x++){wrap[x].classList.add('visible')}})
 
- let btn1=querySelector('#btn1').innertext= username;
+close_btn.addEventListener('click', function(){for(x=0;x<wrap.length;x++){wrap[x].classList.remove('visible')}}) 
 
-
- btn1.addEventListener('click', function(){input.innertext==''; if(input.innerhtml=username){console.log('architecture')}})*/
-
- /*let inputs=document.querySelector('input');
-
- let username={name:olufemi,course: Architecture, Age:34}
-
- function save(){inputs.innerHTML=username;}*/
-
- let input1=document.querySelector('#input1');
-
- let input2=document.querySelector('#input2');
-
- let answer1=document.querySelector('#answer1');
-
- let answer2=document.querySelector('#answer2');
-
- let btn1=document.querySelector('#btn1');
-
- let reset1=document.querySelector('#reset1');
-
- btn1.addEventListener('click', function(){if(input1.value.trim()=='' && input2.value.trim()==''){alert('Fill the empty space')} else{answer1.innerHTML=input1.value.trim(); answer2.innerHTML=input2.value.trim()}})
-
- reset1.addEventListener('click', function(){input1.value=''; input2.value=''; answer1.innerHTML=0; answer2.innerHTML=0;})
-
- /*let username=['power', 'building', 'architect', 'surveyor'];
-
- input1.value.target=username;*/
